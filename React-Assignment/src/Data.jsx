a492ba99bfce50bf95d8f4429ecacbcefb6e96ca
Vivek Shenoy <83587933+vivekshenoy05@users.noreply.github.com>
2022-10-26 23:00:11 +0530

    Add files via upload

---
+++ React-Assignment/src/Data.jsx
@@ -0,0 +1,46 @@
+const DATA = [
+  {
+    id: 0,
+    title: "Nike Ericsion",
+    price: 750,
+    desc: "Short sleeve shirts for men are the best call for this summer season. This shirt style makes It a perfect piece of outfit for a walk around the streets or strolling through the park. You can use it with shorts or pair it up with a pair of denim. If it’s distressed jeans, better. Use sneakers, loafers, or typical canvas shoes with this shirt style and you’ll be in the best summer outfit.",
+    img: "/assets/images/products/p1.jpg",
+  },
+  {
+    id: 1,
+    title: "Casual",
+    price: 800,
+    desc: "You can play more with the long sleeve shirts. This garment has the leverage to be used on almost any occasion. You can wear a long sleeve shirt on formal or informal events and occasion and call it a win. If you like playing around with colors then choose the cool colors and if it’s a formal occasion, then use a single color. In case you find it overwhelming, use white color as that’s the color of all life.",
+    img: "/assets/images/products/p2.jpg",
+  },
+  {
+    id: 2,
+    title: "Full Sleeve",
+    price: 950,
+    desc: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+    img: "/assets/images/products/p3.jpg",
+  },
+  {
+    id: 3,
+    title: "Hunter Shirt",
+    price: 1050,
+    desc: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+    img: "/assets/images/products/p4.jpg",
+  },
+  {
+    id: 4,
+    title: "Beverycrazy shirt",
+    price: 1150,
+    desc: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+    img: "/assets/images/products/p6.png",
+  },
+  {
+    id: 5,
+    title: "Harley Shirt",
+    price: 1250,
+    desc: "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+    img: "/assets/images/products/p7.png",
+  },
+];
+
+export default DATA;
